6451fa8ceefa400c979ed37c6be67e9acf2d292d
root <root@DESKTOP-U89L7C3.localdomain>
2026-06-18 14:58:41 +0300

    string interpolation

---
+++ JS/Intro/templateString.js
@@ -0,0 +1,22 @@
+/*
+string interpolation is a way to combine data
+and strings for output.
+*/
+
+
+
+let firstName="Enoch"
+let secondName="Mungiria"
+let age=20
+let address="kiambu county"
+
+let userDetails= firstName+" " +secondName+" is "+age+" years old "
++" he stays at "+address
+
+console.log(userDetails)
+console.log("User details is",typeof userDetails)
+
+let userDetails2=
+`${firstName} ${secondName} is ${age} years old and he stays at ${address}`;
+console.log(userDetails2)
+console.log(`${firstName} ${secondName} is ${age} years old and he stays at ${address}`)
