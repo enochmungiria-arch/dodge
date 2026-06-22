06bc4b44220c4e02a22c875dc79bf401d21d64e2
root <root@DESKTOP-U89L7C3.localdomain>
2026-06-22 10:10:11 +0300

    more comparison operators

--- JS/Intro/comparisonOperators.js
+++ JS/Intro/comparisonOperators.js
@@ -22,10 +22,32 @@ console.log(`numb1= ${numb1}===numb2=${numb2}
     let numb3=77
     let numb4="77"
     let strictInequal=numb3!==numb4
-    console.log(`numb3 is equal to ${numb3}!==numb4 which is equal to ${numb4}
+    console.log(`numb3 is ${numb3}!==numb4 which is not equal to ${numb4}
         Ans is ${strictInequal} and its type is ${typeof strictInequal} 
     `)
 
-    let nonStrictInequal=numb3!=numb4
-    console.log(`numb3 is ${numb3} != numb4 which is ${numb4} Ans is ${nonStrictInequal}
-        and its type is ${typeof nonStrictInequal}`)
+    let Inequal=numb3!=numb4
+    console.log(`numb3 is ${numb3} != numb4 which is ${numb4} Ans is ${Inequal}
+        and its type is ${typeof Inequal}`)
+
+
+let numb5=51
+let numb6=10
+let str1="a"
+let str2="b"
+console.log (`numb5=${numb5} and numb6=${numb6}`)
+console.log (`str1=${str1} and str2=${str2}`)
+
+console.log (`numb5>numb6 ${numb5>numb6}`)
+console.log (`str1>str2 ${str1>str2}`)
+
+console.log (`numb<numb6 ${numb5<numb6}`)
+console.log (`str1<str2 ${str1<str2}`)
+
+console.log(`numb5>=numb6 ${numb5>=numb6}`)
+console.log(`str1>=str2   ${str1>=str2}`)
+
+console.log(`numb5<=numb6 ${numb5<=numb6}`)
+console.log(`str1<=str2   ${str1<=str2}`)
+
+
