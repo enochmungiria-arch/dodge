71d8b48d4e8d67634bb78462d97cdfe48b7bd742
root <root@DESKTOP-U89L7C3.localdomain>
2026-06-24 15:41:43 +0300

    else if

---
+++ JS/Intro/ifElseIf.js
@@ -0,0 +1,51 @@
+
+/*
+let age=prompt("enter your age")
+console.log=(`age is${age} and its type is 
+    ${typeof age}`);
+alert(`you entered ${age}`)
+
+if(age>10){
+    alert('you are a baby')}
+
+else if(age>13){
+    alert("you are a teenager")}
+
+else if(age>18){
+    alert("You are legally allowed to drink")}
+
+else if (age>27){
+    alert("best age for marriage")}
+
+else if(age>45){
+    alert("best age for retirement")}
+
+else if(age>60){
+    alert("you are aged")}
+*/
+
+    // else if potrays the first truth then stops 
+
+let age=prompt("enter your age")
+console.log=(`age is${age} and its type is 
+    ${typeof age}`);
+alert(`you entered ${age}`)
+
+if(age<10){
+    alert('you are a baby')}
+
+else if(age>13<18){
+    alert("you are a teenager")}
+
+else if(age<18){
+    alert("You are legally allowed to drink")}
+
+else if (age<27){
+    alert("best age for marriage")}
+
+else if(age<45){
+    alert("best age for retirement")}
+
+else if(age<60){alert
+    "you are an ancestor"
+}
