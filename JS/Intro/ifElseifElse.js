01dbe2ffd5471ffcd08b9781ae9cf0b766439294
root <root@DESKTOP-U89L7C3.localdomain>
2026-06-24 11:12:51 +0300

    if

--- JS/Intro/ifElseifElse.js
+++ JS/Intro/ifElseifElse.js
@@ -7,15 +7,21 @@ you can use it on its own
 let age=prompt("enter your age")
 alert(`you entered ${age}`)
 
-if(age>10){'you are a baby'}
+if(age>10){
+    alert('you are a baby')}
 
 
-if(age>13){"you are a teenager"}
+if(age>13){
+    alert("you are a teenager")}
 
-if(age>18){"You are legally allowed to drink"}
+if(age>18){
+    alert("You are legally allowed to drink")}
 
-if (age>27){"best age for marriage"}
+if (age>27){
+    alert("best age for marriage")}
 
-if(age>45){"best age for retirement"}
+if(age>45){
+    alert("best age for retirement")}
 
-if(age>60){"you are aged"}
+if(age>60){
+    alert("you are aged")}
