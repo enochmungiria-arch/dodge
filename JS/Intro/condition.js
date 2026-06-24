fff59bed252e0ef4a018c57f0c5d521a3fef11df
root <root@DESKTOP-U89L7C3.localdomain>
2026-06-24 10:55:06 +0300

    if statements

---
+++ JS/Intro/condition.js
@@ -0,0 +1,41 @@
+/*
+they are:
+1. if
+2. else if
+3. else
+4. tertiary operators
+5. switch
+
+if the condition is a truthy then it will execute code
+in the /{}/
+if it is a falsey itwill not execute inside the{}
+
+*/
+
+let opt1="make money"
+if(opt1){
+    console.log("scenario 1 works")
+}
+
+let opt2=""
+if(opt2){
+    console.log("scenario 2 works")
+
+}
+
+let opt3=20>10
+if (opt3){
+    console.log("scenario 3 works")
+}
+let opt4=true
+if (opt4){
+    console.log("scenario 4 works")
+}
+let opt5=false
+if (opt5){
+    console.log("scenario 5 works")
+}
+let opt6=10>20
+if(opt6){
+    console.log("scenario 6 works")
+}
---
+++ JS/Intro/ifElseifElse.js
@@ -0,0 +1,21 @@
+/* 
+we begin with if
+its special 
+you can use it on its own
+*/
+
+let age=prompt("enter your age")
+alert(`you entered ${age}`)
+
+if(age>10){'you are a baby'}
+
+
+if(age>13){"you are a teenager"}
+
+if(age>18){"You are legally allowed to drink"}
+
+if (age>27){"best age for marriage"}
+
+if(age>45){"best age for retirement"}
+
+if(age>60){"you are aged"}
