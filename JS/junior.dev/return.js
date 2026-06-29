ee7b42595add5aa3d02e6787ac5fd249ce21efa5
root <root@DESKTOP-U89L7C3.localdomain>
2026-06-29 15:14:05 +0300

    return

---
+++ JS/junior.dev/return.js
@@ -0,0 +1,59 @@
+
+/* it is used to return any code  or data
+
+*/
+
+function sayMyName(){
+let firstName = "Samson"
+console.log(`first name is ${firstName}`)
+let secondName = "Jared"
+console.log (`second name is ${secondName}`)
+
+let fullName = (`${firstName} ${secondName}`)
+console.log(`full name is ${fullName}`)
+return false;
+}
+let sayMyNameValue= sayMyName()
+console.log(`function returned ${sayMyNameValue} and its type is${typeof sayMyNameValue}`)
+
+/*
+function sayMyName(){
+    return
+let firstName = "Samson"
+console.log(`first name is ${firstName}`)
+let secondName = "Jared"
+console.log (`second name is ${secondName}`)
+let fullName = (`${firstName} ${secondName}`)
+console.log(`full name is ${fullName}`)
+}
+let sayMyNameValue= sayMyName()
+console.log(`function returned ${sayMyName}`)
+
+/*
+
+function sayMyName(){
+let firstName = "Samson"
+console.log(`first name is ${firstName}`)
+let secondName = "Jared"
+console.log (`second name is ${secondName}`)
+let fullName = (`${firstName} ${secondName}`)
+console.log(`full name is ${fullName}`)
+}
+let sayMyNameValue= sayMyName()
+console.log(`function returned ${sayMyName}`)
+
+
+
+
+function sayMyName(){
+let firstName = "Samson"
+console.log(`first name is ${firstName}`)
+let secondName = "Jared"
+console.log (`second name is ${secondName}`)
+let fullName = (`${firstName} ${secondName}`)
+console.log(`full name is ${fullName}`)
+}
+let sayMyNameValue= sayMyName()
+console.log(`function returned ${sayMyName}`)
+
+*/
