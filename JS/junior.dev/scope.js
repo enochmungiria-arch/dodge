2ea8d26e3a0c5b94a00cceee196bd5ba068bc5a3
root <root@DESKTOP-U89L7C3.localdomain>
2026-06-30 15:42:57 +0300

    fix

--- JS/junior.dev/scope.js
+++ JS/junior.dev/scope.js
@@ -1,11 +1,12 @@
 let name = "Enoch"
 
 function sayMyName(){
+    
 let name = "Maggy";
-console.log(`The name is ${name} `)
+console.log(`name ${name} line ${5} `)
 if (true){
     let name = "Delilah"
-    console.log(`name ${name} `)
+    console.log(`name ${name} line ${8} `)
 
 }
 console.log (`name ${name}`)
