279c22f91053a6784364956a923f346d8a6a33f8
root <root@DESKTOP-U89L7C3.localdomain>
2026-06-30 15:30:00 +0300

    global scope

---
+++ JS/junior.dev/scope.js
@@ -0,0 +1,15 @@
+let name = "Enoch"
+
+function sayMyName(){
+let name = "Maggy";
+console.log(`The name is ${name} `)
+if (true){
+    let name = "Delilah"
+    console.log(`name ${name} `)
+
+}
+console.log (`name ${name}`)
+
+}
+
+sayMyName();
