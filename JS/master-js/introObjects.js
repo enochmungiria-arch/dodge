b955678a11abb507d6adef84e96b95972a048fd7
root <root@DESKTOP-U89L7C3.localdomain>
2026-07-09 20:04:34 +0300

    objects

--- JS/master-js/introObjects.js
+++ JS/master-js/introObjects.js
@@ -25,38 +25,35 @@ value = "enoch"(string)
 //why objects are useful.
 //1. if you want to represent a complex datatype
 
-//Best developersa re lazy developers😂
+//Best developersa re lazy developers
 //onjects easen work of a developer
 
 const car={
-  make:function Properties(){  
-   name="Dodge Challenger srt hellcat"
-   model="srt hellcat redeye"
+  make:{  
+   name :"Dodge Challenger srt hellcat",
+   model:"srt hellcat redeye"
 },
 
-engine :function specs(){
- type:"6.2 litre supercharged HEMI v8"
-induction:"supercharged"
- horsepower:797
- top_speed=240
+engine :{
+ type:"6.2 litre supercharged HEMI v8",
+ induction:"supercharged",
+ horsepower:797,
+ top_speed:240
 
 },
-manufacture:function Details(){
-     manufacturer:"stallantis"
+manufacture:{
+     manufacturer:"stallantis",
      country:"Otario, Canada"
 
 },
 
-  overall : function total ()
-  {
-45000="number of units sold"
-},
+  
   
   alert_info:function(){
-    alert(`car ${car.make}
-        engine ${car.engine}
-        manufacture ${car.manufacture}
-        overall ${car.overall}
+    alert(`car ${car.name}
+        engine ${car.model}
+        
+        
         
         `)
 
@@ -66,3 +63,31 @@ manufacture:function Details(){
   United states of America`
 
 }
+console.log(`car data type ${typeof car} `);
+console.log (car)
+console.log(`Using console.log (table)--`)
+console.table(car)
+
+console.log(`Name is ${car.make.name} and its type is ${typeof car.make.name}`)
+console.log(`model is ${car.make.model} and its type is ${typeof car.make.model}`)
+console.log(`type  is ${car.engine.type} and its type is ${typeof car.engine.type}`)
+console.log(`induction is ${car.engine.induction} and its type is ${typeof car.engine.induction}`)
+console.log(`manufacturer is ${car.manufacture.manufacturer} and its type is ${typeof car.manufacture.manufacturer}`)
+console.log(`country of production is ${car.manufacture.country} and its type is ${typeof car.manufacture.country}`)
+
+console.log(`Name is ${car["make"]["name"]} and its type is ${typeof car["make"]["name"]}`)
+console.log(`model  is ${car["make"]["model"]} and its type is ${typeof car["make"]["model"]}`)
+console.log(`engine type  is ${car["engine"]["type"]} and its type is ${typeof car["engine"]["type"]}`)
+console.log(`car induction is ${car["engine"]["induction"]} and its type is ${typeof car["engine"]["induction"]}`)
+console.log(`car manufacturers are ${car["manufacture"]["manufacturer"]} and its type is ${typeof car["manufacture"]["manufacturer"]}`)
+console.log(`car country of production  is ${car["manufacture"]["country"]} and its type is ${typeof car["manufacture"]["country"]}`)
+
+const x=car.engine
+
+
+
+console.log(`engine is ${x} and its type is ${typeof x}`)
+
+//the bracket notation is an universal accessor
+
+
